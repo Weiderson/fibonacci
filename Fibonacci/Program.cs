ee4ea17e142afb6fc8a75beda8683eab1fb98931
weiderson <weiderson.mendes@gmail.com>
2015-07-24 17:22:14 -0300

    Versao 0.1

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Sequencia de Fibonacci Recursiva (Recursive Fibonacci Sequence)
+ * 
+ */
+
+using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com o valor de N: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if ( (n >= 1) && (n <= 10) )
+            {
+                Console.WriteLine("Fibonacci da sequencia de " + n + " números é: ");
+                Fib(0, 1, 1, n);
+                Console.Read();
+            }
+        }
+
+        static void Fib(int y, int z, int cont, int numero)
+        {           
+            Console.WriteLine(y);
+            if (cont < numero)
+                Fib(z, y + z, cont + 1, numero);
+        }
+    }
+}
